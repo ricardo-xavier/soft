3180d2db21507676e31cb1cf11801a1124da0b4e
Ricardo Xavier <ricardo.costa.xavier@gmail.com>
2021-08-01 17:15:29 +0000

    excel

--- tools/excel.go
+++ tools/excel.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,22 +15,79 @@ type Produto struct {
 	Preco string
 }
 
-func (produto Produto) toString() string {
+func (produto Produto) ToString() string {
 	return fmt.Sprintf("[%s] [%s] [%s]", produto.Codigo, produto.SubCodigo, produto.Preco)
 }
 
 var produtos []Produto
+var produtosBd []Produto
+var m map[string]Produto
 
 func main() {
+	m = make(map[string]Produto)
+	carregaBd()
 	for a:=1; a<len(os.Args); a++ {
 		carregaCsv(os.Args[a])
 	}
+	comparaPrecos()
+}
+
+func comparaPrecos() {
+	encontrados := 0
+	alteradosComPreco := 0
+	alteradosSemPreco := 0
+
+	for _, produtoBd := range(produtosBd) {
+		chave := produtoBd.Codigo + ":" + produtoBd.SubCodigo
+		//fmt.Printf("= [%s]\n", chave)
+		produto, encontrado := m[chave]
+		if encontrado {
+			encontrados++
+			if produto.Preco != produtoBd.Preco {
+				if produtoBd.Preco == "0.00" {
+					alteradosSemPreco++
+				} else {
+					alteradosComPreco++
+					precoBd,_ := strconv.ParseFloat(produtoBd.Preco, 64)
+					precoPlanilha,_ := strconv.ParseFloat(produto.Preco, 64)
+					if math.Abs(precoBd - precoPlanilha) > 0.02 {
+						fmt.Println(produto.ToString() + " " + produtoBd.Preco)
+					}
+				}
+				fmt.Printf("update PRECOS set VLR_PRECO=%s where COD_PARCEIRO='TECNOFLEX' and COD_TABELA='PADRAOJUL2021' and COD_PRODUTO='%s' and SUB_CODIGO='%s';\n", produto.Preco, produto.Codigo, produto.SubCodigo)
+			}
+		}
+	}
+
+	fmt.Printf("Produtos no banco...........: %d\n", len(produtosBd))
+	fmt.Printf("Produtos na planilha........: %d\n", len(produtos))
+	fmt.Printf("Produtos encontrados........: %d\n", encontrados);
+	fmt.Printf("Produtos alterados com preco: %d\n", alteradosComPreco);
+	fmt.Printf("Produtos alterados sem preco: %d\n", alteradosSemPreco);
+}
+
+func carregaBd() {
+	file, err := os.Open("precos.txt")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		words := strings.Split(text, ";")
+		var produto Produto
+		produto.Codigo = strings.TrimSpace(words[0])
+		produto.SubCodigo = strings.TrimSpace(words[1])
+		produto.Preco = strings.TrimSpace(words[2])
+		produtosBd = append(produtosBd, produto)
+	}
+	file.Close();
 }
 
 func carregaCsv(csv string) {
 
 	fmt.Println(csv)
-	file, err := os.Open(os.Args[1])
+	file, err := os.Open(csv)
 	if err != nil {
 		panic(err)
 	}
@@ -40,12 +99,13 @@ func carregaCsv(csv string) {
 		for _, word := range(words) {
 			if word == "Código" {
 				subCodigos := getSubCodigos(words)
-				getProdutos(scanner, subCodigos)
-/*
+				carregaTabela(scanner, subCodigos)
 				for _, produto := range(produtos) {
-					fmt.Println(produto.toString())
+					//fmt.Println(produto.toString())
+					chave := produto.Codigo + ":" + produto.SubCodigo
+					//fmt.Printf("+ [%s]\n", chave)
+					m[chave] = produto
 				}
-*/
 			}
 		}
 	}
@@ -58,7 +118,7 @@ func getSubCodigos(words []string) []string {
 	var subCodigos []string
 	for _, sub := range(words) {
 		sub = strings.TrimSpace(sub)
-		if sub == "Código" || sub == "Descrição" || sub == "" {
+		if sub == "Código" || sub == "Descrição" || sub == "Dimensão" || sub == "" {
 			continue
 		}
 		subCodigos = append(subCodigos, sub)
@@ -69,7 +129,7 @@ func getSubCodigos(words []string) []string {
 	return subCodigos
 }
 
-func getProdutos(scanner *bufio.Scanner, subCodigos []string) {
+func carregaTabela(scanner *bufio.Scanner, subCodigos []string) {
 	for scanner.Scan() {
 		text := scanner.Text()
 		words := strings.Split(text, ";")
@@ -91,7 +151,7 @@ func getProdutos(scanner *bufio.Scanner, subCodigos []string) {
 			break
 		}
 		if len(subCodigos) != len(precos) {
-			panic(text)
+			break
 		}
 		p := 0
 		for _, subCodigo := range(subCodigos) {
